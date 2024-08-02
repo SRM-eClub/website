ed5309fcd2a52b2fdb4680c553b8633e8445e8b0
SRM Electronics Club <160060147+srmeclub@users.noreply.github.com>
2024-08-02 17:38:04 +0530

    fix: optional for types

--- src/components/user_card.tsx
+++ src/components/user_card.tsx
@@ -8,11 +8,11 @@ const poppins = Poppins({
 });
 
 type User = {
-  name: string;
-  email: string;
-  linkedin: string;
+  name?: string;
+  email?: string;
+  linkedin?: string;
   photo: string;
-  description: string;
+  description?: string;
 };
 
 export default async function UserCard(user: User) {
@@ -23,11 +23,11 @@ export default async function UserCard(user: User) {
           <Image src={user.photo} alt={user.name} width={300} height={300} className="rounded-full scale-none hover:scale-105 duration-500"/>
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{user.name}</h2>
-          <p>{user.description}</p>
+          <h2 className="card-title">{user.name || ""}</h2>
+          <p>{user.description || ""}</p>
           <div className="card-actions justify-center py-2 grid grid-cols-2">
-            <Link href={user.linkedin} className="btn btn-primary"  target="_blank">Linkedin</Link>
-            <Link href={"mailto:" + user.email} className="btn btn-primary" target="_blank">Email</Link>
+            <Link href={user.linkedin || ""} className="btn btn-primary"  target="_blank">Linkedin</Link>
+            <Link href={"mailto:" + user.email || ""} className="btn btn-primary" target="_blank">Email</Link>
           </div>
         </div>
       </div>
